ee9091f350c0d45896f50044442abe3696eae633
ginkyo <c.eaton@outlook.com>
2016-04-09 01:09:07 +0100

    push all res to git

--- DragController.java
+++ DragController.java
@@ -1,194 +1,96 @@
 package com.wglxy.example.dragdrop;
 
-/*
- * Copyright (C) 2013 Wglxy.com
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * (Note to other developers: The above note says you are free to do what you want with this code.
- *  Any problems are yours to fix. Wglxy.com is simply helping you get started. )
- */
-
 import android.content.ClipData;
-import android.util.Log;
 import android.view.DragEvent;
 import android.view.View;
 
-/**
- * It handles all drag events generated during a drag-drop operation.
- * When a drag starts, it creates a special view (a DragView) that moves around the screen
- * until the user ends the drag. As feedback to the user, this object causes the device to
- * vibrate as the drag begins. It interacts with other objects through methods defined
- * in the DropTarget and DragSource interfaces.
- * 
- */
-
-public class DragController 
-    implements View.OnDragListener
+public class DragController
+        implements View.OnDragListener
 {
+    private DragSource mDragSource;
 
-    private boolean mDragging;            // indicates that drag-drop is in progress
-    private boolean mDropSuccess;         // indicates that the drop was successful
+    public DragController(){
 
-    private DragSource mDragSource;       // where the drag originated
-    private DropTarget mDropTarget;       // where the object was dropped
-
-
-public DragController(){
-
-}
-/**
- */
-// Methods
-
-/**
- * Handle a drag event for a view.
- * 
- * @param v View
- * @param event DragEvent
- * @return boolean
- */
-
-@Override public boolean onDrag (View v, DragEvent event) {
-
-    // Determine if the view is a DragSource, DropTarget, or both.
-    // That information is used below when the event is handled.
-    boolean isDragSource = false, isDropTarget = false;
-    DragSource source = null;
-    DropTarget target = null;
-    try {
-        source = (DragSource) v;
-        isDragSource = true;
-    } catch (ClassCastException ex) {}
-    try {
-        target = (DropTarget) v;
-        isDropTarget = true;
-    } catch (ClassCastException ex) {}
-
-    boolean eventResult = false;
-    final int action = event.getAction();
-
-    // Handles each of the expected events
-    switch(action) {
-
-      case DragEvent.ACTION_DRAG_STARTED:
-        //Log.d (DragActivity.LOG_NAME, "Drag started");
-
-        // We want a call to mPresenter.onDragStarted once. So check to see if we are already dragging.
-        if (!mDragging) {
-           mDragging = true;
-           mDropSuccess = false;
-           ImageCell ic = (ImageCell) mDragSource;
-           Log.d (DragActivity.LOG_NAME, "Drag started. mDragSource drawable: " + ic.getDrawable ());
-
-        }
-
-        // At the start of a drag, all drop targets must say they are interested in the rest
-        // of the drag events of this drag-drop operation.
-        // Allow for the case where a view is both a source and a target.
-        if (isDragSource) {
-           // The view continues to see drag events if it is the source of the current drag
-           // or if it is a target itself.
-           if (source == mDragSource) {
-              if (source.allowDrag ()) {
-                 eventResult = true;
-                 source.onDragStarted ();
-              }
-           } else {
-             eventResult = isDropTarget && target.allowDrop (mDragSource);
-           }
-        } else {
-            eventResult = isDropTarget && target.allowDrop(mDragSource);
-        }
-        break;
-
-      case DragEvent.ACTION_DRAG_ENTERED:
-        Log.d (DragActivity.LOG_NAME, "DragController.onDrag - entered view");
-        if (isDropTarget) {
-           target.onDragEnter (mDragSource);
-           mDropTarget = target;
-           eventResult = true;
-        } else eventResult = false;
-        break;
-
-      case DragEvent.ACTION_DRAG_EXITED: 
-        Log.d (DragActivity.LOG_NAME, "DragController.onDrag - exited view");
-        if (isDropTarget) {
-           mDropTarget = null;
-           target.onDragExit (mDragSource);
-           eventResult = true;
-        } else eventResult = false;
-        break;
-
-      case DragEvent.ACTION_DROP: 
-        Log.d (DragActivity.LOG_NAME, "DragController.onDrag - dropped");
-        //   Log.d (DragActivity.LOG_NAME, "Drag dropped. mDragSource drawable: " + ic2.getDrawable ());
-        if (isDropTarget) {
-           if (target.allowDrop (mDragSource)) {
-              target.onDrop (mDragSource);
-              mDropTarget = target;
-              mDropSuccess = true;
-           }
-           eventResult = true;
-        } else eventResult = false;
-        break;
-
-      case DragEvent.ACTION_DRAG_ENDED:
-        Log.d (DragActivity.LOG_NAME, "DragController.onDrag - ended");
-        if (mDragging) {
-           // At the end of the drag, do two things.
-           // (1) Inform the drag source that the drag is over; (2) Inform the presenter.
-           Log.d (DragActivity.LOG_NAME, "DragController.onDrag DragSource: " + mDragSource);
-           if (mDragSource != null) mDragSource.onDropCompleted (mDropTarget, mDropSuccess);
-           eventResult =  true;
-        }
-        mDragging = false;
-        mDragSource = null;
-        mDropTarget = null;
-        break;
     }
-    return eventResult;
+    @Override public boolean onDrag (View v, DragEvent event) {
 
-} // end onDragEvent
+        boolean isDragSource = false, isDropTarget = false;
+        DragSource source = null;
+        DropTarget target = null;
+        try {
+            source = (DragSource) v;
+            isDragSource = true;
+        } catch (ClassCastException ex) {}
+        try {
+            target = (DropTarget) v;
+            isDropTarget = true;
+        } catch (ClassCastException ex) {}
 
-/**
- * If the view is a DragSource that allows a drag, start a drag-drop operation.
- *
- * @return boolean - true means a drag-drop operation started
- */    
+        boolean eventResult = false;
+        final int action = event.getAction();
 
-public boolean startDrag (View v) {
+        switch(action) {
 
-    boolean isDragSource = false;
-    DragSource ds = null;
-    try {
-        ds = (DragSource) v;
-        isDragSource = true;
-    } catch (ClassCastException ex) {
+            case DragEvent.ACTION_DRAG_STARTED:
+
+                if (isDragSource) {
+                    if (source == mDragSource) {
+                        if (source.allowDrag ()) {
+                            eventResult = true;
+                        }
+                    } else {
+                        eventResult = isDropTarget && target.allowDrop (mDragSource);
+                    }
+                } else {
+                    eventResult = isDropTarget && target.allowDrop(mDragSource);
+                }
+                break;
+            case DragEvent.ACTION_DRAG_ENTERED:
+                if (isDropTarget) {
+                    target.onDragEnter (mDragSource);
+                    eventResult = true;
+                } else eventResult = false;
+                break;
+
+            case DragEvent.ACTION_DRAG_EXITED:
+                if (isDropTarget) {
+                    target.onDragExit (mDragSource);
+                    eventResult = true;
+                } else eventResult = false;
+                break;
+
+            case DragEvent.ACTION_DROP:
+                if (isDropTarget) {
+                    if (target.allowDrop (mDragSource)) {
+                        target.onDrop (mDragSource);
+                    }
+                    eventResult = true;
+                } else eventResult = false;
+                break;
+
+            case DragEvent.ACTION_DRAG_ENDED:
+
+                break;
+        }
+        return eventResult;
     }
-    if (!isDragSource) return false;
-    if (!ds.allowDrag ()) return false;
+    public boolean startDrag (View v) {
 
-    mDragging = false;
-    mDropSuccess = false;
-    mDragSource = ds;
-    mDropTarget = null;
+        boolean isDragSource = false;
+        DragSource ds = null;
+        try {
+            ds = (DragSource) v;
+            isDragSource = true;
+        } catch (ClassCastException ex) {
+        }
+        if (!isDragSource) return false;
+        if (!ds.allowDrag ()) return false;
 
-    ClipData dragData = ds.clipDataForDragDrop ();
-    View.DragShadowBuilder shadowView = new View.DragShadowBuilder (v);
-    v.startDrag (dragData, shadowView, null, 0);
-    return true;
-}
+        mDragSource = ds;
 
-
+        ClipData dragData = ClipData.newPlainText("","");
+        View.DragShadowBuilder shadowView = new View.DragShadowBuilder (v);
+        v.startDrag (dragData, shadowView, ds, 0);
+        return true;
+    }
 }
